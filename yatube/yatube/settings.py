aaf630ce2c50adaf0d05181be38286eaf31f5050
Александр Бебякин <lorpaxx@yandex.ru>
2021-11-29 11:46:34 +0300

    review прошёл, немного подредактировались

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -30,8 +30,13 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     '[::1]',
     'testserver',
+    'www.kleric.pythonanywhere.com',
+    'kleric.pythonanywhere.com',
 ]
 
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
 
 # Application definition
 
@@ -46,7 +51,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'django_extensions',
+    'django_extensions',
+    'debug_toolbar',
     'sorl.thumbnail',
 ]
 
@@ -58,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
